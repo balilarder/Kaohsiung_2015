8f4dd25fa6bf9c3e1e76b569bc62199169cc92c5
balilarder <balilarder@yahoo.com.tw>
2016-05-03 16:20:13 +0800

    analyze case

--- expect.py
+++ expect.py
@@ -6,7 +6,7 @@ def debug (i):
 	print probability_list[i]
 f1 = open("check.txt", 'r')
 lines = f1.readlines()
-#slicing string to from neibors
+#section 1:slicing string to from neibors
 for i in range(len(lines)):
 	each_list = []
 	start = 0
@@ -25,6 +25,8 @@ for i in range(len(lines)):
 		each_list.append(dict)
 	probability_list.append(each_list)
 f1.close()
+
+
 
 #section 2: read table and convert to list form
 import csv
@@ -45,11 +47,34 @@ for i in range(1,len(lines)):
 	 			for key in probability_list[i-1][each]:
 	 				if key == update:
 	 					probability_list[i-1][each][key] = float(influence[j])
-	 		#print j
+	 		
+#print probability_list
+#print count	
+f1.close()
 
-print probability_list
-print count	
 
 
+
+
+#section 3: expection(try some different thresholds)
+infected = [0 for x in range(len(id))]	#a bool list record infected or not, and to compute similarity in real data
+f1 = open('time-space.csv', 'r')
+count = 0
+#goal: find infected in time[3](= line[3])
+lines = f1.readlines()
+col_name = lines[0].strip().split('\t')[1:]
+
+#case = lines[28].strip().split('\t')[1:]
+case = lines[5].strip().split('\t')[1:]
+
+for i in range(len(case)):
+	if int(case[i]) > 0:
+		#print col_name[i]
+		for each in range(len(id)):
+			if id[each] == col_name[i]:
+				infected[each] = 1
+				print id[each]
+				print probability_list[each]
+#print infected
 
 f1.close()

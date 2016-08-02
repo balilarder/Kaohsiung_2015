364e2351767225831a83e787a924d51d85014a89
balilarder <balilarder@yahoo.com.tw>
2016-08-02 23:59:54 +0800

    combine 2 class to build information table

--- deg1_2.py
+++ deg1_2.py
@@ -1,19 +1,32 @@
 """
 read neighbor information, and form a graph of 1,2 degree of neighbor
 to a dict "list_1_2_deg"
+
+combine AreaInfo with Cell to build a table maintain information
 """
 import csv
 import re
+
+class Cell(object):
+	def __init__(self):
+		self.pv_u_0 = 0
+		self.tau = 1
+		self.Av2u = 0
+		self.credit_a = []
+	def __repr__(self):
+		return "p is %s. tau is %s. Av2u is %s. credit_a is %s." %  \
+    	(self.pv_u_0, self.tau, self.Av2u, self.credit_a)
+
 class AreaInfo(object):
 	def __init__(self):
 		self.Av = 0
-		self.deg1Av2u = {}
-		self.deg2Av2u = {}
-		self.toself = 0
+		self.deg1 = {}	# ex:{'A6432-0108-00': Cell1, 'A6432-0104-00': Cell2}
+		self.deg2 = {}
+		self.toself = Cell()
 	def show(self):
 		print self.Av
-		print self.deg1Av2u 
-		print self.deg2Av2u 
+		print self.deg1 
+		print self.deg2 
 		print self.toself
 		
 
@@ -38,11 +51,11 @@ list_1_2_deg= {}
 for i in range(1, total_areas):
 	segment1 = lines_1[i].split(",")
 	segment1.pop()
-	list_1_2_deg[segment1[0]] = AreaInfo()
+	list_1_2_deg[segment1[0]] = AreaInfo()	# create an object information of area
 	
 	
 	for j in range(1, len(segment1)):
-		list_1_2_deg[segment1[0]].deg1Av2u[segment1[j]] = 0
+		list_1_2_deg[segment1[0]].deg1[segment1[j]] = Cell()
 	
 
 for i in range(1, total_areas):
@@ -52,7 +65,7 @@ for i in range(1, total_areas):
 	
 	
 	for j in range(1, len(segment2)):
-		list_1_2_deg[segment2[0]].deg2Av2u[segment2[j]] = 0
+		list_1_2_deg[segment2[0]].deg2[segment2[j]] = Cell()
 
 
 print len(list_1_2_deg)
